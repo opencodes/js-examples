398f960254921ef0942fa8486e2f980e9429cfa8
Rajesh Kumar Jha <rkjha.it.in@gmail.com>
2016-04-08 12:54:31 +0530

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,4 +1,32 @@
 $(document).ready(function() {
+    jQuery.fn.DataTableExt.oApi.fnFilterClear  = function ( oSettings )
+{
+	var i, iLen;
+
+	/* Remove global filter */
+	oSettings.oPreviousSearch.sSearch = "";
+
+	/* Remove the text of the global filter in the input boxes */
+	if ( typeof oSettings.aanFeatures.f != 'undefined' )
+	{
+		var n = oSettings.aanFeatures.f;
+		for ( i=0, iLen=n.length ; i<iLen ; i++ )
+		{
+			$('input', n[i]).val( '' );
+		}
+	}
+
+	/* Remove the search text for the column filters - NOTE - if you have input boxes for these
+	 * filters, these will need to be reset
+	 */
+	for ( i=0, iLen=oSettings.aoPreSearchCols.length ; i<iLen ; i++ )
+	{
+		oSettings.aoPreSearchCols[i].sSearch = "";
+	}
+
+	/* Redraw */
+	oSettings.oApi._fnReDraw( oSettings );
+};
     var table = $('#example').DataTable( {
         "ajax": "js/objects.txt",
         "columns": [
@@ -42,7 +70,7 @@ $(document).ready(function() {
             .search( searchText)
             .draw();
         }else{
-            table.state.clear();
+            table.fnFilterClear();
         }
     } );
 });
